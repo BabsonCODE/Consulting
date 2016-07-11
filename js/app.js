9f9f88e836e5c91980bc697a3913c754ddb9b338
Arpit Bansal <abansal3@babson.edu>
2016-07-11 14:09:25 +0530

    Completed updates
    Added Angular Routing to make single page application
    Added email feature to contact us
    Added nav bar highlighting on select

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('consulting',['ngRoute']);
+var app = angular.module('consulting',['ngRoute','ngResource']);
 
 app.config(function ($routeProvider) {
 
@@ -30,6 +30,52 @@ app.config(function ($routeProvider) {
     })
 });
 
-app.controller('mainController', ['$scope',function($scope) {
+app.controller('mainController', ['$scope','$log','$location','$resource',function($scope,$log,$location,$resource) {
+
+    $scope.success = false;
+
+    $scope.color = {
+        whoWeAre: '',
+        whatWeDo : '',
+        whatWeHaveDone: '',
+        whatWeOffer: ''
+    };
+
+    function navbar (location) {
+        if (location == '/whoWeAre') {
+            $scope.color.whoWeAre = "#960400";
+            $scope.color.whatWeDo = "rgba(255,255,255,.5)";
+            $scope.color.whatWeHaveDone = "rgba(255,255,255,.5)";
+            $scope.color.whatWeOffer = "rgba(255,255,255,.5)";
+        } else if (location == '/whatWeDo') {
+            $scope.color.whoWeAre = "rgba(255,255,255,.5)";
+            $scope.color.whatWeHaveDone = "rgba(255,255,255,.5)";
+            $scope.color.whatWeOffer = "rgba(255,255,255,.5)";
+            $scope.color.whatWeDo = "#960400";
+        } else if (location == '/whatWeHaveDone') {
+            $scope.color.whoWeAre = "rgba(255,255,255,.5)";
+            $scope.color.whatWeOffer = "rgba(255,255,255,.5)";
+            $scope.color.whatWeDo = "rgba(255,255,255,.5)";
+            $scope.color.whatWeHaveDone = "#960400";
+        } else if (location == '/whatWeOffer') {
+            $scope.color.whoWeAre = "rgba(255,255,255,.5)";
+            $scope.color.whatWeHaveDone = "rgba(255,255,255,.5)";
+            $scope.color.whatWeDo = "rgba(255,255,255,.5)";
+            $scope.color.whatWeOffer = "#960400";
+        } else {
+            $scope.color.whoWeAre = "rgba(255,255,255,.5)";
+            $scope.color.whatWeHaveDone = "rgba(255,255,255,.5)";
+            $scope.color.whatWeDo = "rgba(255,255,255,.5)";
+            $scope.color.whatWeOffer = "rgba(255,255,255,.5)";
+        }
+    }
+
+    $scope.$on('$routeChangeSuccess', function (scope, next, current) {
+        navbar($location.url());
+    });
+
+    if ($location.search().email === "success") {
+        $scope.success = true;
+    }
 
 }]);
